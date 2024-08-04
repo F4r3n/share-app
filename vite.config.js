82f69f2053b0a0968eefa717cbee59861f9da0ce
f4r3n <guillaume_k@hotmail.fr>
2024-08-04 09:38:53 +0200

    update tauri version

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,27 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { internalIpV4 } from "internal-ip";
 import { purgeCss } from 'vite-plugin-tailwind-purgecss';
-const mobile = !!/android|ios/.exec(process.env.TAURI_ENV_PLATFORM);
+const host = process.env.TAURI_DEV_HOST;
+
 export default defineConfig({
   define: {
-    __IS_MOBILE__: mobile,
+    __IS_MOBILE__: !!/android|ios/.exec(process.env.TAURI_ENV_PLATFORM),
   },
 	plugins: [sveltekit(), purgeCss()],
 	 // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
   // 1. prevent vite from obscuring rust errors
   clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
   server: {
-    port: 5173,
+    host: host || false,
+    port: 1420,
     strictPort: true,
-    host: mobile ? "0.0.0.0" : false,
-    hmr: mobile
+    hmr: host
       ? {
-          protocol: "ws",
-          host: await internalIpV4(),
-          port: 1421,
+          protocol: 'ws',
+          host: host,
+          port: 1430,
         }
       : undefined,
-    watch: {
-      // 3. tell vite to ignore watching `src-tauri`
-      ignored: ["**/src-tauri/**"],
-    },
-}
+  },
 });
